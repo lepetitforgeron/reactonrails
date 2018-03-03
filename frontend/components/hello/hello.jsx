7b8c7bb3e83fd6dd712777ad3289bfbfe476aea2
Arne Kleinschmidt <arne.kleinschmidt@9elements.com>
2018-03-03 22:50:52 +0100

    introduces styled components

--- frontend/components/hello/hello.jsx
+++ frontend/components/hello/hello.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-import './hello.css';
+const HelloCard = styled.div`
+  display: inline-block;
+  margin: 32px;
+  border-radius: 9px;
+  padding: 16px 32px;
+  color: #ffffff;
+  background-color: #bfd8d2;
+  font-size: 1.8rem;
+`;
 
 export default class Hello extends React.PureComponent {
   render() {
     return (
-      <div className='hello'>
-        <p className='hello__name'>Hello, I am {this.props.name}!</p>
-        <p className='hello__count'>My count is {this.props.count}.</p>
-        <p className='hello__description'>This is me: {this.props.description}</p>
-      </div>
+      <HelloCard>
+        <p>Hello, I am {this.props.name}!</p>
+        <p>My count is {this.props.count}.</p>
+        <p>This is me: {this.props.description}</p>
+      </HelloCard>
     );
   }
 }
